56a46a4c9b9c957f8a27f776b80b79890235e85d
NicoLaHayeTech <schmidt.nantes@gmail.com>
2025-07-19 22:35:19 +0200

    Update flask_prompts_api.py

--- flask_prompts_api.py
+++ flask_prompts_api.py
@@ -1,4 +1,5 @@
 from flask import Flask, send_from_directory
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,5 @@ def get_prompts():
     return send_from_directory(directory='.', path='prompts.json', mimetype='application/json')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
